5cf23c4ad8c7c3cde4838c8e738135626c45109f
longarms07 <ecridd@gmail.com>
2019-02-25 18:48:47 -0500

    Started work on database accessor

--- monyy/main.py
+++ monyy/main.py
@@ -2,16 +2,34 @@ from monyy import app
 from .database import db, User
 from .login import *
 from flask import Flask, render_template, redirect
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, login_required, logout_user
 
 
 @app.route("/")
 def hello():
-    return "Hello "+current_user.get_username()+"!"
+    if current_user.is_authenticated:
+        return "Hello "+current_user.get_username()+"!"
+    else:
+        return "Hello mlemlem!"
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect("/")
+    form = LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = GetUser(form.username.data, form.password.data)
+        except Exception as error:
+            print(error)
+            return redirect("/login")
+        login_user(user, remember=form.remember_me.data)
+        return redirect("/")
+    return render_template('login_test.html', title='Sign In', form=form)
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
     if current_user.is_authenticated:
         return redirect("/")
     form = LoginForm()
@@ -27,4 +45,9 @@ def login():
     return render_template('login_test.html', title='Sign In', form=form)
 
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
 
